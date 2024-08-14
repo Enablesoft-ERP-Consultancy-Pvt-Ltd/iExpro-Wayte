2bd62fc1af0c2dcf45f19e7a06446c5e037c693f
Prashant <prashantrahul141@protonmail.com>
2024-08-14 11:39:31 +0530

    add 404 and home login page

---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+const FourZeroFourPage = () => {
+  return <div>404</div>;
+};
+
+export default FourZeroFourPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const HomeLoginPage = () => {
+  return <div>login page</div>;
+};
+
+export default HomeLoginPage;

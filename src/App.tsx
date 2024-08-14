8f2dde4011c738919f1a2bc227d0ea317af9d860
Prashant <prashantrahul141@protonmail.com>
2024-08-14 10:19:13 +0530

    introducing zustand
    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { createSignal } from "solid-js";
 import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import {Button} from "~/components/ui/button"
 import "./app.css";
 
 function App() {
@@ -18,13 +17,13 @@ function App() {
       <h1>Welcome to Tauri!</h1>
 
       <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" class="logo vite" alt="Vite logo" />
         </a>
-        <a href="https://tauri.app" target="_blank">
+        <a href="https://tauri.app" target="_blank" rel="noreferrer">
           <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
         </a>
-        <a href="https://solidjs.com" target="_blank">
+        <a href="https://solidjs.com" target="_blank" rel="noreferrer">
           <img src={logo} class="logo solid" alt="Solid logo" />
         </a>
       </div>

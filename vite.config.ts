a5292e437e610960c27d1734a217ae9081ef3f65
Prashant <prashantrahul141@protonmail.com>
2024-08-14 10:30:09 +0530

    introducing felte and zod
    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import path from "path"
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -22,7 +22,7 @@ export default defineConfig(async () => ({
 
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src")
-    }
-  }
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
 }));

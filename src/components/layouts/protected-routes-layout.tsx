f890e3cf3796be6108cc11ca72f69d5c2a063f17
Prashant <prashantrahul141@protonmail.com>
2024-08-15 17:19:02 +0530

    add navbar to protected layout

--- src/components/layouts/protected-routes-layout.tsx
+++ src/components/layouts/protected-routes-layout.tsx
@@ -1,10 +1,11 @@
 import type { RouteSectionProps } from "@solidjs/router";
 import type { Component } from "~/lib/types";
+import Navbar from "~/components/composables/navbar/navbar";
 
 const ProtectedRoutesLayout: Component<RouteSectionProps> = (props) => {
   return (
-    <div>
-      <p>LAYOUT</p>
+    <div class="flex">
+      <Navbar />
       {props.children}
     </div>
   );

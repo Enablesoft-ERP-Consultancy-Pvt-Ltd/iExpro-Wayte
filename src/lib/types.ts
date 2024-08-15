be4c0f5f263c9261f0691035c022edd720b4a4ea
Prashant <prashantrahul141@protonmail.com>
2024-08-15 20:54:56 +0530

    add isService to TProtectedROute page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,9 +2,10 @@ import type { IconTypes } from "solid-icons";
 import type { JSX } from "solid-js";
 
 export type Component<P = Record<string, never>> = (props: P) => JSX.Element;
-export type TNavBarOption = {
+export type TProtectedRoutePage = {
   title: string;
   url: string;
+  isService: boolean;
   tooltip: string;
   icon: IconTypes;
 };

9079c60417a4dca70f3bdaac9142ec39588bd925
Prashant <prashantrahul141@protonmail.com>
2024-08-15 17:18:41 +0530

    add empty settings page ro router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./app.css";
 import { Router, Route } from "@solidjs/router";
-import FourZeroFourPage from "./pages/404";
-import HomeLoginPage from "./pages";
-import ProtectedRoutesLayout from "./components/layouts/protected-routes-layout";
-import WeightServicePage from "./pages/protected/weight-service";
+import FourZeroFourPage from "~/pages/404";
+import HomeLoginPage from "~/pages/index";
+import ProtectedRoutesLayout from "~/components/layouts/protected-routes-layout";
+import WeightServicePage from "~/pages/protected/weight-service";
+import SettingsPage from "~/pages/protected/settings";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path={"/"} component={HomeLoginPage} />
       <Route path={"/protected"} component={ProtectedRoutesLayout}>
         <Route path={"/weight-service"} component={WeightServicePage} />
+        <Route path={"/settings"} component={SettingsPage} />
       </Route>
       <Route path={"*404"} component={FourZeroFourPage} />
     </Router>

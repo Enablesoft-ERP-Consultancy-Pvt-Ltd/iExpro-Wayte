912253860e5f65185e488321bd2c96025029c090
Prashant <prashantrahul141@protonmail.com>
2024-08-15 17:19:14 +0530

    add utility to check if two urls are equal

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function equalUrls(a: string, b: string) {
+  return (
+    new URL(a, "http://example.com").pathname ===
+    new URL(b, "http://example.com").pathname
+  );
+}

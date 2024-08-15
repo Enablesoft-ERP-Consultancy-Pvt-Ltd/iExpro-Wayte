b920bcedf81f321e8e18a0c2d9cad23480a1299d
Prashant <prashantrahul141@protonmail.com>
2024-08-15 17:18:54 +0530

    add half completed navbar

---
+++ src/components/composables/navbar/navbar-button.tsx
@@ -0,0 +1,27 @@
+import { type Component } from "solid-js";
+import type { TNavBarOption } from "~/lib/types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+import { A, useLocation, } from "@solidjs/router";
+import { Button } from "~/components/ui/button";
+import { equalUrls } from "~/lib/utils";
+
+const NavBarButton: Component<{ option: TNavBarOption }> = ({ option }) => {
+  const location = useLocation();
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <A href={option.url}>
+          <Button class="rounded-full size-12 mx-2" variant={equalUrls(location.pathname, option.url) ? "default" : "secondary"}>
+            {<option.icon size={18} />}{ }
+          </Button>
+        </A>
+      </TooltipTrigger>
+      <TooltipContent>{option.tooltip}</TooltipContent>
+    </Tooltip>
+  );
+};
+export default NavBarButton;
---
+++ src/components/composables/navbar/navbar.tsx
@@ -0,0 +1,30 @@
+import { For } from "solid-js";
+import type { TNavBarOption } from "~/lib/types";
+import NavBarButton from "./navbar-button";
+import { FaSolidWeightScale } from "solid-icons/fa";
+import { IoSettingsSharp } from 'solid-icons/io'
+
+const navbarOptions: TNavBarOption[] = [
+  {
+    title: "Weight service",
+    tooltip: "Weight service",
+    url: "/protected/weight-service",
+    icon: FaSolidWeightScale,
+  },
+];
+
+const Navbar = () => {
+  return (
+    <nav class="flex flex-col h-screen border py-4 bg-background">
+      <ul class="flex flex-col gap-2 flex-grow">
+        <For each={navbarOptions} fallback={<div>Loading...</div>}>
+          {(option) => <NavBarButton option={option} />}
+        </For>
+      </ul>
+
+      <NavBarButton option={{ title: "Settings", tooltip: "App settings", url: "/protected/settings", icon: IoSettingsSharp }} />
+    </nav>
+  );
+};
+
+export default Navbar;

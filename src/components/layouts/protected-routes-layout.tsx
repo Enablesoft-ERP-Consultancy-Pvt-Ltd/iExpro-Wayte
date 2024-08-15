c19102dfe9695af0f000a5dd47cf9a58c809335b
Prashant <prashantrahul141@protonmail.com>
2024-08-15 20:55:46 +0530

    update layout to also have current route

--- src/components/layouts/protected-routes-layout.tsx
+++ src/components/layouts/protected-routes-layout.tsx
@@ -1,12 +1,23 @@
-import type { RouteSectionProps } from "@solidjs/router";
+import { useLocation, type RouteSectionProps } from "@solidjs/router";
 import type { Component } from "~/lib/types";
 import Navbar from "~/components/composables/navbar/navbar";
+import { getCurrentProtectedRouteFromUrl } from "~/lib/utils";
 
 const ProtectedRoutesLayout: Component<RouteSectionProps> = (props) => {
+  const location = useLocation();
+  const currentRoute = () => getCurrentProtectedRouteFromUrl(location.pathname);
+
   return (
-    <div class="flex">
-      <Navbar />
-      {props.children}
+    <div class="flex relative">
+      <aside class="sticky top-0 border max-h-screen">
+        <Navbar />
+      </aside>
+      <main class="w-full">
+        <div class="sticky top-0 bg-background w-full border px-4 py-5">
+          <h1 class="text-xl font-semibold">{currentRoute().title}</h1>
+        </div>
+        {props.children}
+      </main>
     </div>
   );
 };

0ce07794f3305790aa817d65fbede41488e8c3d2
Prashant <prashantrahul141@protonmail.com>
2024-08-15 20:55:15 +0530

    util to get current route page from url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { allProtectedRoutePages } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,4 +12,14 @@ export function equalUrls(a: string, b: string) {
     new URL(a, "http://example.com").pathname ===
     new URL(b, "http://example.com").pathname
   );
+}
+
+export function getCurrentProtectedRouteFromUrl(url: string) {
+  for (const route of Object.values(allProtectedRoutePages)) {
+    if (equalUrls(route.url, url)) {
+      return route;
+    }
+  }
+
+  return allProtectedRoutePages["weight-service"];
 }

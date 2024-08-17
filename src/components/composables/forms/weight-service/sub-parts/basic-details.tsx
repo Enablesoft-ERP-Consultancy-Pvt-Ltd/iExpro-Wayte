5a6a3457a497d3597a710425c564e86f86eb964d
Prashant <prashantrahul141@protonmail.com>
2024-08-17 16:49:13 +0530

    fix(frontend): fix style issue with basic-details, physical-details

--- src/components/composables/forms/weight-service/sub-parts/basic-details.tsx
+++ src/components/composables/forms/weight-service/sub-parts/basic-details.tsx
@@ -21,138 +21,132 @@ const WeightServiceFormBasicDetails: Component<{
   const PLACEHOLDER_ORDER_NUMBER = ["ABC1", "XYZ2", "PQR3", "LMN4"];
 
   return (
-    <fieldset class="border border-border/80 rounded-md w-fit space-y-4 p-6">
-      {/* part 1 */}
-      <div class="flex gap-6">
-        {/* Company name */}
-        <div>
-          <Label for="company_name">Company name</Label>
-          <Combobox
-            name="company_name"
-            onChange={(e) => setFields("company_name", e, true)}
-            options={PLACEHOLDER_COMPANY}
-            placeholder="Company"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Company">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* Vendor name */}
-        <div>
-          <Label for="vendor_name">Vendor name</Label>
-          <Combobox
-            name="vendor_name"
-            onChange={(e) => setFields("vendor_name", e, true)}
-            options={PLACEHOLDER_VENDOR}
-            placeholder="Vendor"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Vendor">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* Order no */}
-        <div>
-          <Label for="order_number">Order no</Label>
-          <Combobox
-            name="order_number"
-            onChange={(e) => setFields("order_number", e, true)}
-            options={PLACEHOLDER_ORDER_NUMBER}
-            placeholder="Order no"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Order no">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/*  recieve date */}
-        <div>
-          <Label for="recieve_date">Recieve date</Label>
-          <DatePicker
-            onChange={(v) => setFields("recieve_date", v, true)}
-            name="recieve_date"
-          />
-        </div>
-
-        {/* item code */}
-        <div>
-          <Label for="item_code">Item code</Label>
-          <TextField name="item_code">
-            <TextFieldInput type="text" placeholder="Item code" />
-          </TextField>
-        </div>
+    <fieldset class="border border-border/80 rounded-md w-full p-6 gap-4 grid grid-cols-fields-col items-start justify-start">
+      {/* Company name */}
+      <div>
+        <Label for="company_name">Company name</Label>
+        <Combobox
+          name="company_name"
+          onChange={(e) => setFields("company_name", e, true)}
+          options={PLACEHOLDER_COMPANY}
+          placeholder="Company"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Company">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
       </div>
 
-      {/* part 2 */}
-      <div class="flex gap-4">
-        {/* gate in no */}
-        <div>
-          <Label for="gate_in_number">Gate in no</Label>
-          <TextField name="gate_in_number">
-            <TextFieldInput type="text" placeholder="Gate in" />
-          </TextField>
-        </div>
+      {/* Vendor name */}
+      <div>
+        <Label for="vendor_name">Vendor name</Label>
+        <Combobox
+          name="vendor_name"
+          onChange={(e) => setFields("vendor_name", e, true)}
+          options={PLACEHOLDER_VENDOR}
+          placeholder="Vendor"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Vendor">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
 
-        {/* challan number */}
-        <div>
-          <Label for="challan_number">Challan no</Label>
-          <TextField name="challan_number">
-            <TextFieldInput type="text" placeholder="Challan" />
-          </TextField>
-        </div>
+      {/* Order no */}
+      <div>
+        <Label for="order_number">Order no</Label>
+        <Combobox
+          name="order_number"
+          onChange={(e) => setFields("order_number", e, true)}
+          options={PLACEHOLDER_ORDER_NUMBER}
+          placeholder="Order no"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Order no">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
 
-        {/* bill no */}
-        <div>
-          <Label for="bill_number">Bill no</Label>
-          <TextField name="bill_number">
-            <TextFieldInput type="text" placeholder="Bill no" />
-          </TextField>
-        </div>
+      {/*  recieve date */}
+      <div>
+        <Label for="recieve_date">Recieve date</Label>
+        <DatePicker
+          onChange={(v) => setFields("recieve_date", v, true)}
+          name="recieve_date"
+        />
+      </div>
 
-        {/*  return date */}
-        <div>
-          <Label for="return_qty_date">Return Qty date</Label>
-          <DatePicker
-            onChange={(v) => setFields("return_qty_date", v, true)}
-            name="return_qty_date"
-          />
-        </div>
+      {/* item code */}
+      <div>
+        <Label for="item_code">Item code</Label>
+        <TextField name="item_code">
+          <TextFieldInput type="text" placeholder="Item code" />
+        </TextField>
+      </div>
 
-        {/* freight charges */}
-        <div>
-          <Label for="freight_charges">Freight charges</Label>
-          <TextField name="freight_charges">
-            <TextFieldInput type="text" placeholder="Freight charges" />
-          </TextField>
-        </div>
+      {/* gate in no */}
+      <div>
+        <Label for="gate_in_number">Gate in no</Label>
+        <TextField name="gate_in_number">
+          <TextFieldInput type="text" placeholder="Gate in" />
+        </TextField>
+      </div>
+
+      {/* challan number */}
+      <div>
+        <Label for="challan_number">Challan no</Label>
+        <TextField name="challan_number">
+          <TextFieldInput type="text" placeholder="Challan" />
+        </TextField>
+      </div>
+
+      {/* bill no */}
+      <div>
+        <Label for="bill_number">Bill no</Label>
+        <TextField name="bill_number">
+          <TextFieldInput type="text" placeholder="Bill no" />
+        </TextField>
+      </div>
+
+      {/*  return date */}
+      <div>
+        <Label for="return_qty_date">Return Qty date</Label>
+        <DatePicker
+          onChange={(v) => setFields("return_qty_date", v, true)}
+          name="return_qty_date"
+        />
+      </div>
+
+      {/* freight charges */}
+      <div>
+        <Label for="freight_charges">Freight charges</Label>
+        <TextField name="freight_charges">
+          <TextFieldInput type="text" placeholder="Freight charges" />
+        </TextField>
       </div>
     </fieldset>
   );
--- src/components/composables/forms/weight-service/sub-parts/item-details.tsx
+++ src/components/composables/forms/weight-service/sub-parts/item-details.tsx
@@ -25,247 +25,238 @@ const WeightServiceFormItemDetails: Component<{
   const PLACEHOLDER_COLOR_NAME = ["Red", "Green", "Blue"];
 
   return (
-    <fieldset class="border border-border/80 rounded-md w-fit space-y-4 p-6">
-      {/* part 1 */}
-      <div class="flex gap-6">
-        {/* item category */}
-        <div>
-          <Label for="item_category">Category</Label>
-          <Combobox
-            name="item_category"
-            onChange={(e) => setFields("item_category", e, true)}
-            options={PLACEHOLDER_CATEGORY}
-            placeholder="Company"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Category">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* item name */}
-        <div>
-          <Label for="item_name">Item name</Label>
-          <Combobox
-            name="item_name"
-            onChange={(e) => setFields("item_name", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Item name"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Item name">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* quality name */}
-        <div>
-          <Label for="quality_name">Quality name</Label>
-          <Combobox
-            name="quality_name"
-            onChange={(e) => setFields("quality_name", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Item name"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Quality name">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* color name */}
-        <div>
-          <Label for="color_name">Color name</Label>
-          <Combobox
-            name="color_name"
-            onChange={(e) => setFields("color_name", e, true)}
-            options={PLACEHOLDER_COLOR_NAME}
-            placeholder="Color name"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Color name">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
+    <fieldset class="border border-border/80 rounded-md w-full p-6 gap-4 grid grid-cols-fields-col items-center justify-center">
+      {/* item category */}
+      <div>
+        <Label for="item_category">Category</Label>
+        <Combobox
+          name="item_category"
+          onChange={(e) => setFields("item_category", e, true)}
+          options={PLACEHOLDER_CATEGORY}
+          placeholder="Company"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Category">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
       </div>
 
-      {/* part 2 */}
-      <div class="flex gap-6">
-        {/* order number */}
-        <div>
-          <Label for="order_number">Order no</Label>
-          <Combobox
-            name="order_number"
-            onChange={(e) => setFields("order_number", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Order number"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Order number">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* godown number */}
-        <div>
-          <Label for="godown_number">Godown no</Label>
-          <Combobox
-            name="godown_number"
-            onChange={(e) => setFields("godown_number", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Godown number"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Godown number">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* bin number */}
-        <div>
-          <Label for="bin_number">Bin no</Label>
-          <Combobox
-            name="bin_number"
-            onChange={(e) => setFields("bin_number", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Bin number"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Bin number">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
-
-        {/* iss lot number */}
-        <div>
-          <Label for="iss_lot_number">Iss lot no</Label>
-          <Combobox
-            name="iss_lot_number"
-            onChange={(e) => setFields("iss_lot_number", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Iss lot no"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Iss lot no">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
+      {/* item name */}
+      <div>
+        <Label for="item_name">Item name</Label>
+        <Combobox
+          name="item_name"
+          onChange={(e) => setFields("item_name", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Item name"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Item name">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
       </div>
 
-      {/* part 3 */}
-      <div class="flex gap-6">
-        {/* rec lot no */}
-        <div>
-          <Label for="rec_lot_number">Rec lot no</Label>
-          <TextField name="rec_lot_number">
-            <TextFieldInput type="text" placeholder="Rec lot no" />
-          </TextField>
-        </div>
+      {/* quality name */}
+      <div>
+        <Label for="quality_name">Quality name</Label>
+        <Combobox
+          name="quality_name"
+          onChange={(e) => setFields("quality_name", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Item name"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Quality name">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
 
-        {/* Bale no */}
-        <div>
-          <Label for="bale_number">Bale no</Label>
-          <TextField name="bale_number">
-            <TextFieldInput type="text" placeholder="Bale no" />
-          </TextField>
-        </div>
+      {/* color name */}
+      <div>
+        <Label for="color_name">Color name</Label>
+        <Combobox
+          name="color_name"
+          onChange={(e) => setFields("color_name", e, true)}
+          options={PLACEHOLDER_COLOR_NAME}
+          placeholder="Color name"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Color name">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
 
-        {/* unit */}
-        <div>
-          <Label for="unit">Unit</Label>
-          <Combobox
-            name="unit"
-            onChange={(e) => setFields("unit", e, true)}
-            options={PLACEHOLDER_ITEM_NAME}
-            placeholder="Unit"
-            itemComponent={(props) => (
-              <ComboboxItem item={props.item}>
-                <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
-                <ComboboxItemIndicator />
-              </ComboboxItem>
-            )}
-          >
-            <ComboboxControl aria-label="Unit">
-              <ComboboxInput />
-              <ComboboxTrigger />
-            </ComboboxControl>
-            <ComboboxContent />
-          </Combobox>
-        </div>
+      {/* order number */}
+      <div>
+        <Label for="order_number">Order no</Label>
+        <Combobox
+          name="order_number"
+          onChange={(e) => setFields("order_number", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Order number"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Order number">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
 
-        {/* for old lot no */}
-        <div class="items-top flex space-x-2 items-end pb-3">
-          <Checkbox
-            name="for_old_lot_number"
-            onChange={(e) => setFields("for_old_lot_number", e, false)}
-          />
-          <div class="grid gap-1.5 leading-none">
-            <Label for="for_old_lot_number">For old lot number</Label>
-          </div>
+      {/* godown number */}
+      <div>
+        <Label for="godown_number">Godown no</Label>
+        <Combobox
+          name="godown_number"
+          onChange={(e) => setFields("godown_number", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Godown number"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Godown number">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
+
+      {/* bin number */}
+      <div>
+        <Label for="bin_number">Bin no</Label>
+        <Combobox
+          name="bin_number"
+          onChange={(e) => setFields("bin_number", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Bin number"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Bin number">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
+
+      {/* iss lot number */}
+      <div>
+        <Label for="iss_lot_number">Iss lot no</Label>
+        <Combobox
+          name="iss_lot_number"
+          onChange={(e) => setFields("iss_lot_number", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Iss lot no"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Iss lot no">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
+
+      {/* rec lot no */}
+      <div>
+        <Label for="rec_lot_number">Rec lot no</Label>
+        <TextField name="rec_lot_number">
+          <TextFieldInput type="text" placeholder="Rec lot no" />
+        </TextField>
+      </div>
+
+      {/* Bale no */}
+      <div>
+        <Label for="bale_number">Bale no</Label>
+        <TextField name="bale_number">
+          <TextFieldInput type="text" placeholder="Bale no" />
+        </TextField>
+      </div>
+
+      {/* unit */}
+      <div>
+        <Label for="unit">Unit</Label>
+        <Combobox
+          name="unit"
+          onChange={(e) => setFields("unit", e, true)}
+          options={PLACEHOLDER_ITEM_NAME}
+          placeholder="Unit"
+          itemComponent={(props) => (
+            <ComboboxItem item={props.item}>
+              <ComboboxItemLabel>{props.item.rawValue}</ComboboxItemLabel>
+              <ComboboxItemIndicator />
+            </ComboboxItem>
+          )}
+        >
+          <ComboboxControl aria-label="Unit">
+            <ComboboxInput />
+            <ComboboxTrigger />
+          </ComboboxControl>
+          <ComboboxContent />
+        </Combobox>
+      </div>
+
+      {/* for old lot no */}
+      <div class="flex gap-2 h-full items-center">
+        <Checkbox
+          name="for_old_lot_number"
+          onChange={(e) => setFields("for_old_lot_number", e, false)}
+        />
+        <div class="grid gap-1.5 leading-none">
+          <Label for="for_old_lot_number">For old lot number</Label>
         </div>
       </div>
     </fieldset>

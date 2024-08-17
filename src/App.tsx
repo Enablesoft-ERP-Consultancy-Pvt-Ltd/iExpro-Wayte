d1fd00005203eadb805667d74f41d367f1d5762d
Prashant <prashantrahul141@protonmail.com>
2024-08-18 00:30:19 +0530

    add(frontend): use theme provider to wrap all routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import HomeLoginPage from "~/pages/index";
 import ProtectedRoutesLayout from "~/components/layouts/protected-routes-layout";
 import WeightServicePage from "~/pages/protected/weight-service";
 import SettingsPage from "~/pages/protected/settings";
+import ThemeProvider from "./components/state/providers/theme-provider";
 
 function App() {
   return (
-    <Router>
-      <Route path={"/"} component={HomeLoginPage} />
-      <Route path={"/protected"} component={ProtectedRoutesLayout}>
-        <Route path={"/weight-service"} component={WeightServicePage} />
-        <Route path={"/settings"} component={SettingsPage} />
-      </Route>
-      <Route path={"*404"} component={FourZeroFourPage} />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Route path={"/"} component={HomeLoginPage} />
+        <Route path={"/protected"} component={ProtectedRoutesLayout}>
+          <Route path={"/weight-service"} component={WeightServicePage} />
+          <Route path={"/settings"} component={SettingsPage} />
+        </Route>
+        <Route path={"*404"} component={FourZeroFourPage} />
+      </Router>
+    </ThemeProvider>
   );
 }
 

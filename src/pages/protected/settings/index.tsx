29f980071f1b26904e5a7154100edb6d0b88607e
Prashant <prashantrahul141@protonmail.com>
2024-08-18 10:22:04 +0530

    update,add(frontend): add baud rate field and fix style issue with settings page

--- src/pages/protected/settings/index.tsx
+++ src/pages/protected/settings/index.tsx
@@ -12,6 +12,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/components/ui/select";
+import {
+  NumberField,
+  NumberFieldDecrementTrigger,
+  NumberFieldIncrementTrigger,
+  NumberFieldInput,
+} from "~/components/ui/number-field";
 
 const SettingsPage = () => {
   const appSettings = useAppSettingsStore();
@@ -42,77 +48,102 @@ const SettingsPage = () => {
   });
 
   return (
-    <form class="p-6 w-full max-w-5xl space-y-4" ref={form}>
-      {/* weight service settings */}
-      <div class="w-full">
-        {/* weight service settings title */}
-        <div class="flex gap-2 items-center">
-          <p class="text-nowrap font-semibold text-primary/80">
-            Weight Service
-          </p>
-          <div class="border border-border w-full" />
-        </div>
+    <div class="w-full flex items-center justify-center">
+      <form class="p-6 w-full max-w-5xl space-y-4" ref={form}>
+        {/* weight service settings */}
+        <div class="w-full">
+          {/* weight service settings title */}
+          <div class="flex gap-2 items-center">
+            <p class="text-nowrap font-semibold text-primary/80">
+              Weight Service
+            </p>
+            <div class="border border-border w-full" />
+          </div>
 
-        {/* ports */}
-        <div class="flex items-center gap-4 w-full p-2 px-5">
-          <div class="flex items-center w-full">
-            <Label class="flex-grow" for="weight_service_settings.port">
-              Port
+          {/* ports */}
+          <div class="flex items-center gap-4 w-full p-2 px-5">
+            <div class="flex items-center w-full">
+              <Label class="flex-grow" for="weight_service_settings.port">
+                Port
+              </Label>
+              <Select
+                value={data((d) => d.weight_service_settings.port)}
+                onChange={(v) => {
+                  setFields("weight_service_settings.port", v, true);
+                }}
+                options={ports()}
+                placeholder="port"
+                itemComponent={(props) => (
+                  <SelectItem item={props.item}>
+                    {props.item.rawValue}
+                  </SelectItem>
+                )}
+              >
+                <SelectTrigger aria-label="Port" class="w-[180px]">
+                  <SelectValue<string>>
+                    {(state) => state.selectedOption()}
+                  </SelectValue>
+                </SelectTrigger>
+                <SelectContent />
+              </Select>
+            </div>
+          </div>
+
+          {/* baud rate */}
+          <div class="flex items-center gap-4 w-full p-2 px-5">
+            <Label for="baud_rate" class="w-full">
+              Baud rate
             </Label>
-            <Select
-              value={data((d) => d.weight_service_settings.port)}
-              onChange={(v) => {
-                setFields("weight_service_settings.port", v, true);
+            <NumberField
+              class="w-36"
+              name="baud_rate"
+              onRawValueChange={(v) => {
+                setFields("weight_service_settings.baud_rate", v, true);
               }}
-              options={ports()}
-              placeholder="port"
-              itemComponent={(props) => (
-                <SelectItem item={props.item}>{props.item.rawValue}</SelectItem>
-              )}
+              defaultValue={appSettings().weight_service_settings.baud_rate}
             >
-              <SelectTrigger aria-label="Port" class="w-[180px]">
-                <SelectValue<string>>
-                  {(state) => state.selectedOption()}
-                </SelectValue>
-              </SelectTrigger>
-              <SelectContent />
-            </Select>
+              <div class="relative">
+                <NumberFieldInput />
+                <NumberFieldIncrementTrigger />
+                <NumberFieldDecrementTrigger />
+              </div>
+            </NumberField>
           </div>
         </div>
-      </div>
 
-      {/* apperance */}
-      <div class="w-full">
-        {/* apperance title */}
-        <div class="flex gap-2 items-center">
-          <p class="text-nowrap font-semibold text-primary/80">Appearance</p>
-          <div class="border border-border w-full" />
+        {/* apperance */}
+        <div class="w-full">
+          {/* apperance title */}
+          <div class="flex gap-2 items-center">
+            <p class="text-nowrap font-semibold text-primary/80">Appearance</p>
+            <div class="border border-border w-full" />
+          </div>
+
+          {/* setting options */}
+          <div class="flex items-center gap-4 w-full p-2 px-5">
+            {/* theme */}
+            <Label for="dark_theme" class="w-full">
+              Dark theme
+            </Label>
+            <Switch
+              defaultChecked={appSettings().appearance.dark_theme}
+              name="dark_theme"
+              onChange={(v) => {
+                setFields("appearance.dark_theme", v, true);
+              }}
+            >
+              <SwitchControl>
+                <SwitchThumb />
+              </SwitchControl>
+            </Switch>
+          </div>
         </div>
 
-        {/* setting options */}
-        <div class="flex items-center gap-4 w-full p-2 px-5">
-          {/* theme */}
-          <Label for="dark_theme" class="w-full">
-            Dark theme
-          </Label>
-          <Switch
-            defaultChecked={appSettings().appearance.dark_theme}
-            name="dark_theme"
-            onChange={(v) => {
-              setFields("appearance.dark_theme", v, true);
-            }}
-          >
-            <SwitchControl>
-              <SwitchThumb />
-            </SwitchControl>
-          </Switch>
+        <div class="w-full flex justify-end py-6 px-3">
+          <Button type="submit">Save</Button>
         </div>
-      </div>
-
-      <div class="w-full flex justify-end py-6 px-3">
-        <Button type="submit">Save</Button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 

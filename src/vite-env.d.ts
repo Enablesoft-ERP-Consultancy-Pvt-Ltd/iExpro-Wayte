cc87ca2722def58249a2841886cefdcd7d9046d1
Prashant <prashantrahul141@protonmail.com>
2024-09-22 14:52:37 +0530

    feat(frontend): type safe import.meta.env taken from env schema

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+type ImportMetaEnvAugmented =
+  import("@julr/vite-plugin-validate-env").ImportMetaEnvAugmented<
+    typeof import("../src/lib/env").default
+  >;
+
+interface ImportMetaEnv extends ImportMetaEnvAugmented {}
